5b5168307c41a66c1129f4160ab6921a0fc7a2da
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2023-03-29 16:30:42 +0530

    Add files via upload

---
+++ src/controller/reg.controller.js
@@ -0,0 +1,56 @@
+import express from "express";
+import Reg from "../models/reg.model.js";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+let newToken = (regData) => {
+  return jwt.sign({ regData }, process.env.JWT_SECRET_KEY);
+};
+
+let router = express.Router();
+router.get("/:id", async (req, res) => {
+  console.log("heree");
+  try {
+    let data = await Reg.findById(req.params.id);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).send("error");
+  }
+});
+
+router.post(
+  "/",
+  //validating the given data using express validator middleware
+
+  body("password").notEmpty().isStrongPassword(),
+  body("email").isEmail(),
+  async (req, res) => {
+    console.log("here");
+    try {
+      let errors = validationResult(req);
+      //if has error
+      if (!errors.isEmpty()) {
+        res.status(404).send({ message: errors.array() });
+      } else {
+        //if validator does not has any error
+        //checking weather user already exists or not
+        let regData = await Reg.findOne({ email: req.body.email });
+        //if already exists =>send error to try another email
+        if (regData) {
+          res.status(200).send("exists");
+          console.log("multiple");
+        } else {
+          regData = await Reg.create(req.body);
+          //creating a token
+          let token = newToken(regData);
+          //sending user details and the token
+          res.status(200).send({ token, regData });
+        }
+      }
+    } catch (error) {
+      res.status(500).send(error);
+      console.log(error);
+    }
+  }
+);
+
+export default router;
---
+++ src/db/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+
+export const connectMongoDB=()=>{
+    return mongoose.connect('');
+}
